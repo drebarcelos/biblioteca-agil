02dd541405128e40c6cd1cb55336749909d1581b
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-01-09 19:55:00 -0300

    Adiciona funcionalidade doar livro
    Quando o usuario digitar a opcao 3 do menu, o programa ira solicitar as
    informacoes do livro que esta sendo doado e caso tudo esteja correto o
    livro sera adicionado a lista em memoria.

--- src/br/com/aceleradora/agil/Biblioteca.java
+++ src/br/com/aceleradora/agil/Biblioteca.java
@@ -2,10 +2,19 @@ package br.com.aceleradora.agil;
 
 import java.util.Scanner;
 
+import br.com.aceleradora.agil.modelo.Livro;
+import br.com.aceleradora.agil.repositorio.RepositorioDeLivros;
+import br.com.aceleradora.agil.repositorio.RepositorioDeLivrosEmMemoria;
+
 public class Biblioteca {
 	private static final Scanner SCANNER = new Scanner(System.in);
 	private static final int OPCAO_INICIAL = 1;
 	private static final int OPCAO_SAIR = 0;
+	private RepositorioDeLivros repositorioDeLivros;
+	
+	public Biblioteca() {
+		this.repositorioDeLivros = new RepositorioDeLivrosEmMemoria();
+	}
 	
 	public void iniciar() {
 		int opcao = OPCAO_INICIAL;
@@ -35,6 +44,9 @@ public class Biblioteca {
 		case 0:
 			sair();
 			break;
+		case 3:
+			doarLivro();
+			break;
 		default:
 			opcaoInvalida();
 			break;
@@ -44,6 +56,31 @@ public class Biblioteca {
 	private void sair() {
 		System.out.println("Programa finalizado com sucesso!");
 		System.exit(0);
+	}
+	
+	private void doarLivro() {
+		int numero;
+		String titulo;
+		String autor;
+		int ano;
+		
+		System.out.println("Digite o numero do livro: ");
+		numero = SCANNER.nextInt();
+		SCANNER.nextLine();
+		
+		System.out.println("Digite o título do livro: ");
+		titulo = SCANNER.nextLine();
+		
+		System.out.println("Digite o autor do livro: ");
+		autor = SCANNER.nextLine();
+		
+		System.out.println("Digite o ano do livro: ");
+		ano = SCANNER.nextInt();
+		
+		Livro livro = new Livro(numero, titulo, autor, ano);
+		repositorioDeLivros.adicionaLivro(livro);
+		
+		System.out.println(livro);
 	}
 	
 	private void opcaoInvalida() {
